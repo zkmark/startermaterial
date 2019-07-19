075c573105723c800b5f54b72024843a8d8909f8
ztvmark <zkmark9999@gmail.com>
2019-07-18 22:45:30 -0500

    Eliminados y movidos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainMenuComponent } from './components/main-menu/main-menu.component';
+
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+
 import { HomeComponent } from './pages/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { MenuItemComponent } from './components/menu-item/menu-item.component';
+import { MenuItemComponent } from './components/menus/menu-item/menu-item.component';
 import { SidenavItemComponent } from './components/menus/sidenav-item/sidenav-item.component';
 
 import { MyIconComponent } from './components/my-icon/my-icon.component';
@@ -20,7 +20,6 @@ import { MyIconComponent } from './components/my-icon/my-icon.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MainMenuComponent,
     HomeComponent,
     MenuItemComponent,
     SidenavItemComponent,
